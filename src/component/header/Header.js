d64f78db149908a77d5c3210a94917171f9fc724
raview69 <raviwicaksono@aol.com>
2022-05-09 11:09:55 +0700

    first commit

---
+++ src/component/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <div>
+                <h1>ReactMeal</h1>
+                <button>Cart</button>
+            </div>
+            <div></div>
+        </>
+    )
+}
+
+export default Header
